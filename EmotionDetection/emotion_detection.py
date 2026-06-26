cc8f88fff566d0359734eabe1645f413891ac4a0
Paylag Torossian <torossian.p@outlook.com>
2026-06-26 06:48:40 -0400

    Add EmotionDetection package

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = {"raw_document": {"text": text_to_analyze}}
+
+    response = requests.post(url, json=input_json, headers=header)
+    formatted_response = json.loads(response.text)
+
+    emotion = formatted_response['emotionPredictions'][0]['emotion']
+    anger = emotion['anger']
+    disgust = emotion['disgust']
+    fear = emotion['fear']
+    joy = emotion['joy']
+    sadness = emotion['sadness']
+
+    emotion_scores = {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness
+    }
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+
+    return {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness,
+        'dominant_emotion': dominant_emotion
+    }
